137a6203f27475725dcb6e1fe97e8b6c091d23a7
Atharva Muley <atharvamuley1@gamil.com>
2018-08-18 15:01:39 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Volgging</title>
+  </head>
+  <body>
+    <p>This website is purely made for logging</p>
+  </body>
+</html>
